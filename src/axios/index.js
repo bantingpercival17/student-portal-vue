1a22ffdacc0b5fbe8a1a44cec38e2b5f649fb01d
bantingpercival17 <percivalbanting@gmail.com>
2023-07-21 07:25:01 +0800

    Website Route

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
 if (process.env.NODE_ENV === 'production') {
-  axios.defaults.baseURL = 'http://one.bma.edu.ph/api/'
+  axios.defaults.baseURL = 'http://20.0.0.120:70/api/'
 } else {
   axios.defaults.baseURL = 'http://127.0.0.1:7000/api/'
   /* axios.defaults.baseURL = 'http://20.0.0.120:70/api/' */
-  /* axios.defaults.baseURL = 'http://192.168.100.6:7070/api/' */
+  /* axios.defaults.baseURL = 'http://192.168.100.59:7000/api/' */
+  axios.defaults.baseURL = 'http://20.0.0.120:70/api/'
 }
