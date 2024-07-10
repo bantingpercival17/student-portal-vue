633b532c6463e020009f1c4d3646b5354035afb4
bantingpercival <percivalbanting@gmail.com>
2024-07-10 14:16:36 +0800

    Website - Content for News and Events

--- src/views/guest/DisciplineAndDrills.vue
+++ src/views/guest/DisciplineAndDrills.vue
@@ -2,9 +2,29 @@
     <image-title-header titleHeader="DISCIPLINE & DRILLS" :banner="require('@/assets/resources/banner/banner-5.jpg')"
         topValue="80%" />
     <div class="container-fluid mt-5">
-        <div class="card">
-            <div class="card-body">
+        <div class="card ">
+            <div class="card-body row">
+                <div class="col-lg-6 col-md-12">
+                    <label for="" class="text-primary fw-bolder display-6">
+                        NORM AND DECOROMS
+                    </label>
+                    <p>
+                        Baliwag Maritime Academy upholds high standards of discipline and professionalism. Students are
+                        expected to adhere to a dress code, maintain punctuality, and exhibit respectful behavior.
+                        Academic integrity is paramount, with a zero-tolerance policy for cheating and plagiarism.
+                        Active participation in classes and training is encouraged to develop essential maritime skills.
+                        Safety protocols and respect for property are strictly enforced. The academy emphasizes
+                        professional conduct, preparing students for successful careers in the maritime industry.
+                    </p>
+                </div>
+                <div class="col-lg-6 col-md-12">
+                    <iframe src="http://bma.edu.ph/assets/video/CZGqsWPcRkuN.mp4" width="560" height="314"
+                        style="border:none;overflow:hidden" scrolling="no" frameborder="0"
+                        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                        allowFullScreen="true"></iframe>
+                </div>
             </div>
+
         </div>
     </div>
 </template>
--- src/views/guest/NewsAndEvents.vue
+++ src/views/guest/NewsAndEvents.vue
@@ -30,24 +30,37 @@ export default {
         ImageTitleHeader
     },
     data() {
-        const dataList = [{
-            titleName: 'THE BALIWAG MARITIME ACADEMY SAYS “NO” TO MAGNA CARTA FOR FILIPINO SEAFARERS CHAPTER 18',
-            author: 'MIDN 3C CRUZ JOHN RAINIEL',
-            frameUrl: `<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fdolphinpubsbma%2Fposts%2F122131589516139401&show_text=true&width=500" width="500" height="715" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write;
+        const dataList = [
+            {
+                titleName: 'Powering the Future with Advanced Engineering Training and Career-Defining Opportunities!',
+                authot: '',
+                frameUrl: '<iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FBaliwagMaritimeAcademyOfficial%2Fvideos%2F1023777155756929%2F&show_text=false&width=560&t=0" width="560" height="314" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>',
+                description: 'Join MIDN 1C Cruz, John Lloyd on His Journey to Becoming a Professional Marine Engineer – Baliwag Maritime Academy Marine Engineering Course: Powering the Future with Advanced Engineering Training and Career-Defining Opportunities!'
+            },
+            {
+                titleName: 'Navigating Success with World-Class Training and Opportunities!',
+                authot: '',
+                frameUrl: '<iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FBaliwagMaritimeAcademyOfficial%2Fvideos%2F482839264137489%2F&show_text=false&width=560&t=0" width="560" height="314" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>',
+                description: 'Join MIDN 1C Uy, Joshua Emmanuel on His Journey to Becoming a Professional Seafarer – Baliwag Maritime Academys Marine Transportation Course: Navigating Success with World-Class Training and Opportunities!'
+            },
+            {
+                titleName: 'THE BALIWAG MARITIME ACADEMY SAYS “NO” TO MAGNA CARTA FOR FILIPINO SEAFARERS CHAPTER 18',
+                author: 'MIDN 3C CRUZ JOHN RAINIEL',
+                frameUrl: `<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fdolphinpubsbma%2Fposts%2F122131589516139401&show_text=true&width=500" width="500" height="715" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write;
              encrypted-media; picture-in-picture; web-share"></iframe>`,
-            description: ` In a powerful display of solidarity, The BMA Administration, Faculty, Staff and the Corps of Midshipmen of the Academy gathered at BMA Grounds today, March 11 2024, to show their unwavering support to the movement of Philippine Association of Maritime Institution (PAMI).
+                description: ` In a powerful display of solidarity, The BMA Administration, Faculty, Staff and the Corps of Midshipmen of the Academy gathered at BMA Grounds today, March 11 2024, to show their unwavering support to the movement of Philippine Association of Maritime Institution (PAMI).
 Alongside the morning parade of the BMA Corps of Midshipmen was the parade of the BMA Administration, Faculty and Staff to show our commitment and this would serve as a silent protest against this provision of the bill that would eventually become a death sentence to the future of the new generation of seafarers.
 This movement of the institution highly supports the opposition against the Magna Carta for Filipino Seafarers specifically Chapter 18 as it poses threats to our Maritime Education Sector.
 In line with this, We shall continually wear a BLACK POLO or TSHIRT to signify and present our stand against this threat that would jeopardize the future of the maritime industry in terms of supply of manpower.
 Photo Credits:
 MIDN 2C Lagunero Rodgen
 MIDN 3C Moya Francis Andrei.`
-        }, {
-            titleName: '3RD DAY CELEBRATION OF 30TH FOUNDATION OF THE BALIWAG MARITIME ACADEMY',
-            author: 'NEWS DEPARTMENT ',
-            frameUrl: `<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fdolphinpubsbma%2Fposts%2F122120339480139401&show_text=true&width=500" width="500" height="696" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay;
+            }, {
+                titleName: '3RD DAY CELEBRATION OF 30TH FOUNDATION OF THE BALIWAG MARITIME ACADEMY',
+                author: 'NEWS DEPARTMENT ',
+                frameUrl: `<iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fdolphinpubsbma%2Fposts%2F122120339480139401&show_text=true&width=500" width="500" height="696" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay;
              clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>`,
-            description: `As part of the celebration, Baliwag Maritime Academy organized a series of fun games for the  faculty, staff, and midshipmen at the BMA grounds on January 17, 2024 at exactly 1400H.
+                description: `As part of the celebration, Baliwag Maritime Academy organized a series of fun games for the  faculty, staff, and midshipmen at the BMA grounds on January 17, 2024 at exactly 1400H.
 At the start of the event, opening remarks were delivered by Ma’am Ma. Conception C. Ong, the Quality Management Representative, who emphasized the importance of celebrating and having fun despite the pressure and stress of both the midshipmen and the faculty members in preparation of course materials. The event kicked off with an array of entertaining games hosted by Sir Ariel Fernando.
 The games started with a breathtaking tug of war in which the winners were the first class midshipmen.
 The second game was charades in which the faculty and staff together with the 3rd class midshipmen were considered as winners.
@@ -58,7 +71,7 @@ PHOTO CREDITS:
 2/C LAGUNERO, RODGEN
 3/C MOYA, FRANCIS ANDREI
 3/C SIA, NASSER NICOLAS III`
-        }]
+            }]
         return {
             dataList
         }
