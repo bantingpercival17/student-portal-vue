d640d7ce18dee50c745929b1db037a45b76b85b8
bantingpercival17 <percivalbanting@gmail.com>
2025-01-03 14:05:21 +0800

    Mobile App Config

--- src/axios/index.js
+++ src/axios/index.js
@@ -5,5 +5,6 @@ if (process.env.NODE_ENV === 'production') {
   // axios.defaults.baseURL = 'http://127.0.0.1:7000/api/'
 } else {
   axios.defaults.baseURL = 'http://127.0.0.1:7000/api/'
+   axios.defaults.baseURL = 'http://one.bma.edu.ph/api/'
   // axios.defaults.baseURL = 'http://20.0.0.80:7000/api/'
 }
